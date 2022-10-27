1c64132c5a41dd8e18066e472f62e7194c170401
Jason Jalufka <jalufka@synthetik-technologies.com>
2022-10-26 21:53:58 -0500

    fix: remove optionsl ssl_support_method

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -73,7 +73,6 @@ resource "aws_cloudfront_distribution" "cloudfront" {
 
   viewer_certificate {
     cloudfront_default_certificate = true
-    ssl_support_method             = "sni-only"
     minimum_protocol_version       = "TLSv1"
   }
 }
